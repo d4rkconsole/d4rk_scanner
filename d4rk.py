56e096e89964ef4b0f12f941f7390d753f28bb9d
d4rk sh4d0w <88644343+d4rkconsole@users.noreply.github.com>
2022-09-13 17:46:15 +0530

    Update d4rk.py

--- d4rk.py
+++ d4rk.py
@@ -9,12 +9,7 @@ banner = pyfiglet.figlet_format(" D4RK  SCANNER ")
 print(banner)
 print('-' * 80)
 print("Time started at : " , datetime.now())
-print("Scanning tool with banner grabbing it will  do :")
-print(" [+]  Tool Created by D4rk sh4d0w : Hacker and programmer ")
-print(" [*] Follow my friend name - yourAnonS0u1  and Lorian synaro ")
-print(""" [+] yourAnonS0u1 Github profile : https://github.com/HLoTW 
- [+]  Lorian synaro twitter : https://twitter.com/loriansynaro """ )
-print('-' * 80)
+
 
 target = input(" [+] Enter the target ip to Scan : ")
 ports = int(input(" [*] Enter how many ports you want to scan : "))
